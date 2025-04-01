004930fc246cb9f9b62b49015e1dd9e313c75bcc
Yeddula Shantan <yshantaninfo@gmail.com>
2025-04-01 12:14:27 +0530

    project setup
    initial commit

---
+++ CompresssorApplication/src/main/java/com/freibu/compresssorapplication/CompresssorApplication.java
@@ -0,0 +1,13 @@
+package com.freibu.compresssorapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompresssorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompresssorApplication.class, args);
+    }
+
+}
